52da0460338d618f7382e5f3d093cf24e2251b30
Jeffrey Nichols <jsnichols@suprocktech.com>
2021-03-12 11:06:09 -0500

    Optimize processing for larger buckets

--- lambda.py
+++ lambda.py
@@ -1,5 +1,6 @@
-import os
 from base64 import b64encode
+import os
+import tempfile
 
 import boto3
 
@@ -8,79 +9,87 @@ list_template = '<li><a href="{path}">{name}</a>'
 
 S3_BUCKET = os.environ.get("S3_BUCKET")
 
-FILE_TYPES = {"egg", "whl", "gz"}
-
-s3 = boto3.resource("s3")
+FILE_TYPES = {".egg", ".whl", ".gz"}
 
 
-def is_allowed_type(path):
-    return os.path.splitext(path)[1][1:] in FILE_TYPES
+def get_all_packages():
+    client = boto3.client('s3')
+    paginator = client.get_paginator('list_objects')
 
+    packages = set()
+    for result in paginator.paginate(Bucket=S3_BUCKET, Delimiter='/'):
+        for prefix in result.get('CommonPrefixes'):
+            packages.add(prefix.get('Prefix').strip("/"))
 
-def get_objects():
-    bucket = s3.Bucket(S3_BUCKET)
-    return {obj.key: obj for obj in bucket.objects.all()}
-
-
-def parse_path(path):
-    package, *filename = path.split("/")
-    return package, "".join(filename)
-
-
-def handler(event, context):
-    objects = get_objects()
-
-    path = event.get("path", "/")[1:]
-    package, filename = parse_path(path)
-
-    if package:
-
-        if filename:
-
-            # download file
-            if path in objects and is_allowed_type(path):
-                summary = objects[path]
-                obj = s3.Object(summary.bucket_name, summary.key)
-                local_path = f"/tmp/{filename}"
-                obj.download_file(local_path)
-                with open(local_path, "rb") as file:
-                    data = file.read()
-                data = b64encode(data)
-                return {
-                    "isBase64Encoded": True,
-                    "headers": {
-                        "Content-Disposition": f"attachment; filename={filename}",
-                        "Content-Type": "application/zip, application/octet-stream",
-                    },
-                    "body": data.decode(),
-                }
-            else:
-                raise Exception("Invalid file requested.")
-
-        # list package files
-        buffer = len(package) + 1
-
-        items = {}
-        for key in objects:
-            print(package, key)
-            filename = key[buffer:] if key.startswith(package) else key
-            print(filename)
-            if "/" not in filename and is_allowed_type(filename):
-                items[filename] = filename
-
-    else:
-
-        # list packages
-        items = {
-            key: key for key in objects if key.endswith("/") and "/" not in key[:-1]
-        }
-
-    listing = sorted(
-        list_template.format(path=key, name=value) for key, value in items.items()
-    )
+    list_items = []
+    for package in sorted(packages):
+        list_items.append(list_template.format(path=package + "/",
+                                               name=package))
 
     return {
         "isBase64Encoded": False,
         "headers": {"Content-Type": "text/html"},
-        "body": page_template.format("\n".join(listing)),
+        "body": page_template.format("\n".join(list_items)),
     }
+
+
+def get_package(package_name):
+    client = boto3.client('s3')
+    paginator = client.get_paginator('list_objects')
+
+    files = set()
+    prefix = f"{package_name}/{package_name}"
+    for result in paginator.paginate(Bucket=S3_BUCKET, Prefix=prefix):
+        for entry in result.get('Contents'):
+            filename = entry.get("Key").split("/", maxsplit=1)[1]
+            if os.path.splitext(filename)[1] in FILE_TYPES:
+                files.add(filename)
+
+    list_items = []
+    for filename in sorted(files):
+        list_items.append(list_template.format(path=filename,
+                                               name=filename))
+
+    return {
+        "isBase64Encoded": False,
+        "headers": {"Content-Type": "text/html"},
+        "body": page_template.format("\n".join(list_items)),
+    }
+
+
+def get_file(path, filename):
+    if os.path.splitext(path)[1] not in FILE_TYPES:
+        raise Exception("Invalid file requested.")
+
+    client = boto3.client('s3')
+
+    # download file
+    with tempfile.TemporaryFile() as f:
+        client.download_fileobj(S3_BUCKET, path, f)
+        f.seek(0)
+        data = b64encode(f.read())
+
+    return {
+        "isBase64Encoded": True,
+        "headers": {
+            "Content-Disposition": f"attachment; filename={filename}",
+            "Content-Type": "application/zip, application/octet-stream",
+        },
+        "body": data.decode(),
+    }
+
+
+def handler(event, context):
+    path = event.get("path", "/")[1:]
+    try:
+        package, filename = path.split("/", maxsplit=1)
+    except ValueError:
+        package = path
+        filename = ""
+
+    if filename:
+        return get_file(path, filename)
+    elif package:
+        return get_package(package)
+    else:
+        return get_all_packages()
